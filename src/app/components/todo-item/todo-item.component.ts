c5f422045a9f6ff08652a588be7a7de4c040dabd
senthilj94 <senthilj94@users.noreply.github.com>
2020-03-25 22:19:50 +0530

    complete, delete functionalities

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Todo } from 'src/app/models/Todo';
+import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-todo-item',
@@ -9,8 +10,9 @@ import { Todo } from 'src/app/models/Todo';
 export class TodoItemComponent implements OnInit {
 
   @Input() todo: Todo;
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter<Todo>();
 
-  constructor() { }
+  constructor(private todoService: TodoService) { }
 
   ngOnInit(): void {
   }
@@ -23,11 +25,16 @@ export class TodoItemComponent implements OnInit {
     return classes;
   }
 
-  toggle() {
-    this.todo.completed = !this.todo.completed;
+  toggle(todo: Todo) {
+    console.log(todo.title);
+    todo.completed = !todo.completed;
+    this.todoService.updateTodo(todo).subscribe(updateResult => {
+      console.log(updateResult);
+    });
   }
 
-  delete() {
+  delete(todo: Todo) {
+    this.deleteTodo.emit(todo);
     console.log('delete');
   }
 }
---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Todo } from '../models/Todo';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class TodoService {
+
+  todoUrl = 'https://jsonplaceholder.typicode.com/todos';
+  todoLimit = '?_limit=10';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getTodo(): Observable<Todo[]> {
+    return this.httpClient.get<Todo[]>(`${this.todoUrl}${this.todoLimit}`);
+  }
+
+  updateTodo(todo: Todo): Observable<any> {
+    const url = `${this.todoUrl}/${todo.id}`;
+    return this.httpClient.put(url, todo, httpOptions);
+  }
+
+  deleteTodo(todo: Todo): Observable<Todo> {
+    const url = `${this.todoUrl}/${todo.id}`;
+    return this.httpClient.delete<Todo>(url, httpOptions);
+  }
+}
